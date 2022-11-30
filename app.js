9f2c1fbf9c4ac517e385dea26b93e74370b1cc71
Semka <iillyyaa907@gmail.com>
2022-11-30 08:47:34 +0700

    added active card selection

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const slides = document.querySelectorAll('.slide')
+function slidesPlugin(activeSlide = 0) {
+  const slides = document.querySelectorAll('.slide')
 
-slides.forEach((slide) => {
-  slide.addEventListener('click', () => {
-    clearActiveClasses()
-    slide.classList.add('active')
-  })
-})
+  slides[activeSlide].classList.add('active')
 
-function clearActiveClasses() {
   slides.forEach((slide) => {
-    slide.classList.remove('active')
+    slide.addEventListener('click', () => {
+      clearActiveClasses()
+      slide.classList.add('active')
+    })
   })
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove('active')
+    })
+  }
 }
+
+slidesPlugin()
